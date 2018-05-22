5fa07c6bb31da3e2c7ff6fb03d6c42f7f7a4aff1
郭新雨 <guo.xinyu1@ztesoft.com>
2018-05-22 14:10:56 +0800

    #1

--- code/scripts/feature/components/flow-chart/view/decorator/CanvasRenderingContext2DDecorator.js
+++ code/scripts/feature/components/flow-chart/view/decorator/CanvasRenderingContext2DDecorator.js
@@ -1,5 +1,5 @@
-class DrawUtil {
-  static splitString2SomeLines(str, linePixelLimit, ctx) {
+class CanvasRenderingContext2DDecorator {
+  static splitString2SomeLines(str, linePixelLimit) {
     const strArray = str.split(/\s+/g);
     var newString = '';
     for (let item of strArray) {
@@ -9,7 +9,7 @@ class DrawUtil {
       let tempStrArray = newString.split('\n');
       let tempLastLine = tempStrArray[tempStrArray.length - 1];
       let tempStr = tempLastLine + ' ' + item;
-      if (ctx.measureText(tempLastLine).width <= linePixelLimit && ctx.measureText(tempStr).width > linePixelLimit) {
+      if (this.measureText(tempLastLine).width <= linePixelLimit && this.measureText(tempStr).width > linePixelLimit) {
         newString += '\n';
       } else {
         newString += ' ';
@@ -18,26 +18,26 @@ class DrawUtil {
     }
     return newString;
   }
-  static cutString(string, pixelLimit, ctx) {
+  static cutString(string, pixelLimit) {
     var cuttedString = '';
     for (let char of string) {
       let tmpStr = cuttedString + char + '…';
-      if (ctx.measureText(tmpStr).width > pixelLimit) {
+      if (this.measureText(tmpStr).width > pixelLimit) {
         return cuttedString + '…';
       }
       cuttedString += char;
     }
     return cuttedString;
   }
-  static fillPath(path, fillStyle, ctx) {
-    ctx.fillStyle = fillStyle;
-    ctx.fill(path);
+  static fillPath(path, fillStyle) {
+    this.fillStyle = fillStyle;
+    this.fill(path);
   }
-  static strokePath(path, strokeStyle, lineWidth, ctx) {
-    ctx.strokeStyle = strokeStyle;
-    ctx.lineWidth = lineWidth;
-    ctx.stroke(path);
+  static strokePath(path, strokeStyle, lineWidth) {
+    this.strokeStyle = strokeStyle;
+    this.lineWidth = lineWidth;
+    this.stroke(path);
   }
 }
 
-export { DrawUtil };
+export { CanvasRenderingContext2DDecorator };
