f6298d4974279c860b5629324ad8c8e4e2cd06a1
郭新雨 <guo.xinyu1@ztesoft.com>
2018-05-24 09:44:26 +0800

    #1

---
+++ code/scripts/feature/components/flow-chart/view/decorator/CompositeDataViewDataDecorator.js
@@ -0,0 +1,7 @@
+import { CompositeData } from '../../model/composite-data/CompositeData.js';
+
+class CompositeDataViewDataDecorator extends CompositeData {
+
+}
+
+export { CompositeDataViewDataDecorator };
