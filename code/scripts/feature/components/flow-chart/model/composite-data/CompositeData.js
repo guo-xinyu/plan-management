39af6cbe93f66530c2afeffdfe4b6dcc50abbf6d
郭新雨 <guo.xinyu1@ztesoft.com>
2018-05-24 15:06:30 +0800

    #1

--- code/scripts/feature/components/flow-chart/model/composite-data/CompositeData.js
+++ code/scripts/feature/components/flow-chart/model/composite-data/CompositeData.js
@@ -3,26 +3,15 @@ import { Composite } from '../../../../../design-pattern-character/composite/Com
 const userData = Symbol();
 
 class CompositeData extends Composite {
-  constructor(type, path, data, nameField, idNumber) {
+  constructor(data) {
     if (data instanceof CompositeData) {
       // 複製構造函數
       super();
       return Object.assign(this, data);
     }
     super(data);
-    this.name = data[nameField];
-    this.path = path;
-    this.animatedSteps = 0;
+    // this.name = data.name;
     this[userData] = data;
-  }
-  addPath(toAddPath) {
-    if (!(toAddPath instanceof Path2D)) {
-      throw new Error('CompositeData的path僅接受Path2D類對象。');
-    }
-    this[userData].viewData.path.addPath(toAddPath);
-  }
-  getPath() {
-    return this[userData].viewData.path;
   }
 }
 
--- code/scripts/feature/components/flow-chart/view/decorator/CompositeDataViewDataDecorator.js
+++ code/scripts/feature/components/flow-chart/view/decorator/CompositeDataViewDataDecorator.js
@@ -1,4 +1,8 @@
 import { CompositeData } from '../../model/composite-data/CompositeData.js';
+import { FactoryViewData } from '../factory-view-data/FactoryViewData.js';
+
+const viewData = Symbol();
+const factoryViewData = Symbol();
 
 class CompositeDataViewDataDecorator extends CompositeData {
   constructor(compositeData) {
@@ -6,6 +10,17 @@ class CompositeDataViewDataDecorator extends CompositeData {
       throw new Error('CompositeDataViewDataDecorator僅可裝飾CompositeData');
     }
     super(compositeData);
+    this[factoryViewData] = new FactoryViewData();
+    this[viewData] = this[factoryViewData].produceViewData(this.getName(), new Path2D());
+  }
+  addPath(toAddPath) {
+    if (!(toAddPath instanceof Path2D)) {
+      throw new Error('CompositeData的path僅接受Path2D類對象。');
+    }
+    this.getPath().addPath(toAddPath);
+  }
+  getPath() {
+    return this[viewData].path;
   }
 }
 
