3ddfd30c842370ee3c697e858c40f13d03d0eaf5
郭新雨 <guo.xinyu1@ztesoft.com>
2018-05-30 17:15:38 +0800

    #1

--- code/scripts/feature/components/flow-chart/view/factory-view-data/workshop-design/TeamClosedPath.js
+++ code/scripts/feature/components/flow-chart/view/factory-view-data/workshop-design/TeamClosedPath.js
@@ -1,63 +1,133 @@
 class TeamClosedPath {
-  fabricateStepUsualSymbolPath(position, radius) {
-    var path = new Path2D();
-    path.arc(position[0], position[1], radius, 0, Math.PI * 2);
-    return path;
+  constructor() {
+    this.fabricateStepSymbolPathState = {
+      usual: Symbol(),
+      transfer: Symbol()
+    };
+  }
+  // fabricateStepUsualSymbolPath(position, radius) {
+  //   var path = new Path2D();
+  //   path.arc(position[0], position[1], radius, 0, Math.PI * 2);
+  //   return path;
+  // }
+
+  get fabricateStepSymbolPathState() {
+    return this.fabricateStepSymbolPathState;
   }
 
-  fabricateStepHasOutputSymbolPath(position, radius, rotatingDegree) {
-    var path = new Path2D();
-    const lineXOffset = radius * Math.sin(rotatingDegree);
-    const lineYOffset = radius * Math.cos(rotatingDegree);
-    const pathPoint1 = [
-      position[0] - lineXOffset,
-      position[1] + lineYOffset
-    ];
-    const pathPoint2 = [
-      pathPoint1[0] + (2 * lineXOffset),
-      pathPoint1[1] - (2 * lineYOffset)
-    ];
-    const pathPoint3 = [
-      pathPoint2[0] + (2 * lineYOffset),
-      pathPoint2[1] + (2 * lineXOffset)
-    ];
-    const pathPoint4 = [
-      pathPoint3[0] - (2 * lineXOffset),
-      pathPoint3[1] + (2 * lineYOffset)
-    ];
-    const controlPoint1 = [
-      pathPoint1[0] - lineYOffset,
-      pathPoint1[1] - lineXOffset
-    ];
-    const controlPoint2 = [
-      controlPoint1[0] + lineXOffset,
-      controlPoint1[1] - lineYOffset
-    ];
-    path.moveTo(pathPoint1[0], pathPoint1[1]);
-    path.arcTo(controlPoint1[0], controlPoint1[1], controlPoint2[0], controlPoint2[1], radius);
-    const controlPoint3 = [
-      controlPoint2[0] + lineXOffset,
-      controlPoint2[1] - lineYOffset
-    ];
-    path.arcTo(controlPoint3[0], controlPoint3[1], pathPoint2[0], pathPoint2[1], radius);
-    path.lineTo(pathPoint3[0], pathPoint3[1]);
-    const controlPoint4 = [
-      pathPoint3[0] + lineYOffset,
-      pathPoint3[1] + lineXOffset
-    ];
-    const controlPoint5 = [
-      controlPoint4[0] - lineXOffset,
-      controlPoint4[1] + lineYOffset
-    ];
-    path.arcTo(controlPoint4[0], controlPoint4[1], controlPoint5[0], controlPoint5[1], radius);
-    const controlPoint6 = [
-      controlPoint5[0] - lineXOffset,
-      controlPoint5[1] + lineYOffset
-    ];
-    path.arcTo(controlPoint6[0], controlPoint6[1], pathPoint4[0], pathPoint4[1], radius);
-    path.closePath();
-    return path;
+  fabricateStepSymbolPath(position, radius, state, rotatingDegree) {
+    let path = new Path2D();
+    if (state === this.fabricateStepSymbolPathState.usual) {
+      path.arc(position[0], position[1], radius, 0, Math.PI * 2);
+      return path;
+    } else if (state === this.fabricateStepSymbolPathState.transfer) {
+      const lineXOffset = radius * Math.sin(rotatingDegree);
+      const lineYOffset = radius * Math.cos(rotatingDegree);
+      const pathPoint1 = [
+        position[0] - lineXOffset,
+        position[1] + lineYOffset
+      ];
+      const pathPoint2 = [
+        pathPoint1[0] + (2 * lineXOffset),
+        pathPoint1[1] - (2 * lineYOffset)
+      ];
+      const pathPoint3 = [
+        pathPoint2[0] + (2 * lineYOffset),
+        pathPoint2[1] + (2 * lineXOffset)
+      ];
+      const pathPoint4 = [
+        pathPoint3[0] - (2 * lineXOffset),
+        pathPoint3[1] + (2 * lineYOffset)
+      ];
+      const controlPoint1 = [
+        pathPoint1[0] - lineYOffset,
+        pathPoint1[1] - lineXOffset
+      ];
+      const controlPoint2 = [
+        controlPoint1[0] + lineXOffset,
+        controlPoint1[1] - lineYOffset
+      ];
+      path.moveTo(pathPoint1[0], pathPoint1[1]);
+      path.arcTo(controlPoint1[0], controlPoint1[1], controlPoint2[0], controlPoint2[1], radius);
+      const controlPoint3 = [
+        controlPoint2[0] + lineXOffset,
+        controlPoint2[1] - lineYOffset
+      ];
+      path.arcTo(controlPoint3[0], controlPoint3[1], pathPoint2[0], pathPoint2[1], radius);
+      path.lineTo(pathPoint3[0], pathPoint3[1]);
+      const controlPoint4 = [
+        pathPoint3[0] + lineYOffset,
+        pathPoint3[1] + lineXOffset
+      ];
+      const controlPoint5 = [
+        controlPoint4[0] - lineXOffset,
+        controlPoint4[1] + lineYOffset
+      ];
+      path.arcTo(controlPoint4[0], controlPoint4[1], controlPoint5[0], controlPoint5[1], radius);
+      const controlPoint6 = [
+        controlPoint5[0] - lineXOffset,
+        controlPoint5[1] + lineYOffset
+      ];
+      path.arcTo(controlPoint6[0], controlPoint6[1], pathPoint4[0], pathPoint4[1], radius);
+      path.closePath();
+      return path;
+    }
+    throw new Error('繪製步驟標志僅有“普通（usual）”、“中轉（transfer）”兩種狀態。');
   }
+
+  // fabricateStepHasOutputSymbolPath(position, radius, rotatingDegree) {
+  //   var path = new Path2D();
+  //   const lineXOffset = radius * Math.sin(rotatingDegree);
+  //   const lineYOffset = radius * Math.cos(rotatingDegree);
+  //   const pathPoint1 = [
+  //     position[0] - lineXOffset,
+  //     position[1] + lineYOffset
+  //   ];
+  //   const pathPoint2 = [
+  //     pathPoint1[0] + (2 * lineXOffset),
+  //     pathPoint1[1] - (2 * lineYOffset)
+  //   ];
+  //   const pathPoint3 = [
+  //     pathPoint2[0] + (2 * lineYOffset),
+  //     pathPoint2[1] + (2 * lineXOffset)
+  //   ];
+  //   const pathPoint4 = [
+  //     pathPoint3[0] - (2 * lineXOffset),
+  //     pathPoint3[1] + (2 * lineYOffset)
+  //   ];
+  //   const controlPoint1 = [
+  //     pathPoint1[0] - lineYOffset,
+  //     pathPoint1[1] - lineXOffset
+  //   ];
+  //   const controlPoint2 = [
+  //     controlPoint1[0] + lineXOffset,
+  //     controlPoint1[1] - lineYOffset
+  //   ];
+  //   path.moveTo(pathPoint1[0], pathPoint1[1]);
+  //   path.arcTo(controlPoint1[0], controlPoint1[1], controlPoint2[0], controlPoint2[1], radius);
+  //   const controlPoint3 = [
+  //     controlPoint2[0] + lineXOffset,
+  //     controlPoint2[1] - lineYOffset
+  //   ];
+  //   path.arcTo(controlPoint3[0], controlPoint3[1], pathPoint2[0], pathPoint2[1], radius);
+  //   path.lineTo(pathPoint3[0], pathPoint3[1]);
+  //   const controlPoint4 = [
+  //     pathPoint3[0] + lineYOffset,
+  //     pathPoint3[1] + lineXOffset
+  //   ];
+  //   const controlPoint5 = [
+  //     controlPoint4[0] - lineXOffset,
+  //     controlPoint4[1] + lineYOffset
+  //   ];
+  //   path.arcTo(controlPoint4[0], controlPoint4[1], controlPoint5[0], controlPoint5[1], radius);
+  //   const controlPoint6 = [
+  //     controlPoint5[0] - lineXOffset,
+  //     controlPoint5[1] + lineYOffset
+  //   ];
+  //   path.arcTo(controlPoint6[0], controlPoint6[1], pathPoint4[0], pathPoint4[1], radius);
+  //   path.closePath();
+  //   return path;
+  // }
 
   fabricateRectanglePath(position, width, height) {
     var path = new Path2D();
--- code/scripts/feature/components/flow-chart/view/factory-view-data/workshop-design/TeamDesign.js
+++ code/scripts/feature/components/flow-chart/view/factory-view-data/workshop-design/TeamDesign.js
@@ -1,5 +1,73 @@
-class TeamDesign {
+import { TeamClosedPath } from './TeamClosedPath.js';
 
+function fabricatePatternDesign(path, width, style, fillColor, strokeColor) {
+  return {
+    border: {
+      path: path,
+      style: style,
+      width: width
+    },
+    color: {
+      fill: fillColor,
+      stroke: strokeColor
+    }
+  };
+}
+
+function fabricateTextDesign(path, width, style, fillColor, strokeColor) {
+  return {
+    text: '',
+    foulInterval: [
+      [],
+      []
+    ],
+    size: 'px'
+  };
+}
+
+const teamClosedPath = Symbol();
+
+class TeamDesign {
+  constructor() {
+    this[teamClosedPath] = new TeamClosedPath();
+  }
+  // 生産節點名字處的“綫稿”
+  fabricateSNodeDesign() {
+    let patternDesign = [];
+    let textDesign = [];
+    // 文字區域的“綫稿”
+    patternDesign.push(fabricatePatternDesign(this[teamClosedPath].fabricateRectanglePath()));
+    textDesign.push(fabricateTextDesign());
+    // 豎綫裝飾的“綫稿”
+    patternDesign.push(fabricatePatternDesign(this[teamClosedPath].fabricateRectanglePath()));
+    // 步驟軌道的“綫稿”
+    patternDesign.push(fabricatePatternDesign(this[teamClosedPath].fabricateRectanglePath()));
+    return { textDesign, patternDesign };
+  }
+  // 生産步驟處的“綫稿”
+  fabricateStepDesign() {
+    let patternDesign = [];
+    let textDesign = [];
+    // 文字區域的“綫稿”
+    patternDesign.push(fabricatePatternDesign(this[teamClosedPath].fabricateRectanglePath()));
+    textDesign.push(fabricateTextDesign());
+    // 圓圈標志的“綫稿”
+    patternDesign.push(fabricatePatternDesign(this[teamClosedPath].fabricateRectanglePath()));
+    return { textDesign, patternDesign };
+  }
+  // 生産輸出節點處的“綫稿”
+  fabricateOutputNodeDesign() {
+    let patternDesign = [];
+    let textDesign = [];
+    // 文字區域的“綫稿”
+    patternDesign.push(fabricatePatternDesign(this[teamClosedPath].fabricateRectanglePath()));
+    textDesign.push(fabricateTextDesign());
+    // 分叉軌道實綫部分的“綫稿”
+    patternDesign.push(fabricatePatternDesign(this[teamClosedPath].fabricateRectanglePath()));
+    // 分叉軌道虚綫部分的“綫稿”
+    patternDesign.push(fabricatePatternDesign(this[teamClosedPath].fabricateRectanglePath()));
+    return { textDesign, patternDesign };
+  }
 }
 
 export { TeamDesign };
